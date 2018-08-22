ade6d9f0033ee53ca5608d2a957eea1388414522
Catherine Lee <catherinelee274@yahoo.com>
2018-08-22 11:58:12 -0700

    shell script

--- downloadimagery.sh
+++ downloadimagery.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-clear
 
-echo "downloading imagery from today..."
+echo "What is your API_ID?"
+read id
 
+export PL_API_KEY=$id
+
+echo "What is the starting date (in yyyy/mm/dd) for your search?"
+read date1
+
+echo "What is the ending date (in yyyy/mm/dd) for your search?"
+read date2
+
+planet data search --item-type PSScene4Band --geom lapaz.geojson --date acquired gte $date1 --date acquired lte $date2 --range cloud_cover lt .1
 
